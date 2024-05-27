239e8ca55357ccfd24cf099234bf9644517242a8
Frozenk <newolf-hackworld@wanadoo.fr>
2024-05-27 19:51:21 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,12 +1,16 @@
 #!/bin/bash
 #Téléchargement du git et install des requirements
+
 sudo mkdir -p /opt/tools
 sudo git clone https://github.com/Frozenka/uploader.git /opt/tools/uploader
 sudo pip3 install -r /opt/tools/uploader/requirements.txt
+
 #Déclaration de l'emplacement
 UPLOADER_FILE="/opt/tools/uploader/uploader.py"
+
 #Formatage de l'alias
 ALIAS_UPLOADER="alias uploader='python3 $UPLOADER_FILE'"
+
 #Recherche du Shell actif
 CURRENT_SHELL="$SHELL"
 case $CURRENT_SHELL in
@@ -24,6 +28,7 @@ case $CURRENT_SHELL in
         exit 1
         ;;
 esac
+
 # Vérification si l'alias existe déjà
 if grep -q "$ALIAS_UPLOADER" "$CURRENT_SHELL"; then
     echo "Alias déjà présent!"
