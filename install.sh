f73b9c98d7ae6f11c5f49027269c585ae67bab89
Frozenk <newolf-hackworld@wanadoo.fr>
2024-05-27 18:08:27 +0200

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+#Téléchargement du git et install des requirements
+git clone https://github.com/Frozenka/uploader.git /opt/tools/uploader
+pip3 install -r /opt/tools/uploader/requirements.txt
+
+#Déclaration de l'emplacement
+UPLOADER_FILE="/opt/tools/uploader/uploader.py"
+#Formatage de l'alias
+ALIAS_UPLOADER="alias uploader='python3 $UPLOADER_FILE"
+#Recherche du Shell actif
+CURRENT_SHELL=$(basename "$SHELL")
+case $CURRENT_SHELL in
+    bash)
+        UPLOADER_FILE="$HOME/.bashrc"
+        ;;
+    zsh)
+        UPLOADER_FILE="$HOME/.zshrc"
+        ;;
+    sh)
+        UPLOADER_FILE="$HOME/.profile"
+        ;;
+    *)
+        echo "Erreur : $CURRENT_SHELL"
+        exit 1
+        ;;
+esac
+#Vérif si l'alias éxiste déja
+if ! grep -Fxq "$ALIAS_UPLOADER" "$UPLOADER_FILE"; then
+    echo "$ALIAS_UPLOADER" >> "$UPLOADER_FILE"
+fi
+#Rechargement du hell
+source "$CURRENT_SHELL"
