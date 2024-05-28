3c1719c670eb2419c1f3156bd92b1da8cd56677e
Frozenk <newolf-hackworld@wanadoo.fr>
2024-05-28 10:54:06 +0200

    Update uploader.py

--- uploader.py
+++ uploader.py
@@ -1,263 +1,174 @@
-★ IntelliCode API Usage Examples
-Thoughts?  Take our Survey!
-Derived from 4 examples found on GitHub
-Real World Examples of simple_term_menu.TerminalMenu.show()
-Grouped by signature:
+#!/usr/bin/env python3
 
-show()
+'''
+Description :
+# Auteur : Frozenk / Christopher SIMON        
+Ce programme permet d'obtenir directement la commande de téléchargement en fonction du système d'exploitation (Windows ou Linux),
+et d'ouvrir automatiquement le serveur Python jusqu'à ce qu'un code 200 soit obtenu
+À ce moment, le serveur Python se fermera.
 
+Todo :
+- Améliorer le style graphique
+- Gérer les erreurs de manière plus efficace
+'''
 
+import os
+import socket
+import argparse
+import readline
+import http.server
+import socketserver
+import subprocess
+import netifaces as ni
+from simple_term_menu import TerminalMenu
+import signal
+import sys
 
+# Gérer l'interruption Ctrl+C
+def signal_handler(sig, frame):
+    print('\nStopping the program ...')
+    sys.exit(0)
 
-Page 1 of 1
+signal.signal(signal.SIGINT, signal_handler)
 
- 
-def menu(title, menu_list):
-    if platform.system() == 'Windows':
-        selection = SelectionMenu.get_selection(menu_list, title=title, show_exit_option=False)
+# Géstion de la syntaxe
+def Syntaxe(OS, IPHOST, selected_file, selected_port):
+    if OS == "Linux":
+        syntaxeFinal = f'wget http://{IPHOST}:{selected_port}/{os.path.basename(selected_file)}'
+    elif OS == "Windows":
+        syntaxeFinal = f'iwr http://{IPHOST}:{selected_port}/{os.path.basename(selected_file)} -O {os.path.basename(selected_file)}'
     else:
-        menu = TerminalMenu(menu_list, title=title)
-        selection = menu.show()
-    return menu_list[selection]
- ShutdownRepo/shellerator/shellerator.py
- 
-def select_dependency(dep, packages):
-    print()
-    if len(packages) == 0:
-        print(
-            bcolors.WARNING +
-            "No package was identified installed on your system for module " +
-            "{}, what you want to do??".format(dep) +
-            bcolors.ENDC
-        )
+        print("/!\ ERROR : unsupported operating system")
+        return
 
-        menu = TerminalMenu(extra_options)
-        selected_idx = menu.show()
+    handler_object = HTTPserv(selected_file, selected_port)
+    try:
+        my_server = socketserver.TCPServer(("", int(selected_port)), handler_object)
+        os.system(f"echo -n {syntaxeFinal} | xclip -selection clipboard | echo 'The command {syntaxeFinal} is in your clipboard'")
+        my_server.serve_forever()
+    except OSError as e:
+        print(f"/!\ ERROR : Unable to start the server on port {selected_port}. {e}")
 
-        if selected_idx == 0:
-            pkg_name = ask_package_input(dep)
-            if pkg_name == "":
-                print(
-                    bcolors.WARNING +
-                    "Module {} ignored.".format(dep) +
-                    bcolors.ENDC
-                )
-                return ""
-            else:
-                print(
-                    bcolors.OKGREEN +
-                    "Using {} as package for module {}".format(pkg_name, dep) +
-                    "." + bcolors.ENDC
-                )
-                return pkg_name
-        else:
-            print(
-                bcolors.WARNING +
-                "Module {} ignored.".format(dep) +
-                bcolors.ENDC
-            )
-            return ""
+# Gestion des ports
+def check_port_in_use(IPHOST, selected_port):
+    # Créer un socket TCP
+    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+    try:
+        sock.bind((IPHOST, selected_port))
+    except OSError:
+        # Le port est déjà utilisé
+        return True
+    # Le port n'est pas accéssible sans plus de droits
+    except PermissionError:
+        print("Permission denied for port {}. Please enter a new port.".format(selected_port))
+        return True
+    sock.close()
+    # Le port n'est pas utilisé
+    return False
+
+# Gestion du serveur
+def HTTPserv(selected_file, selected_port):
+    print("Starting server")
+
+    class Requester(http.server.SimpleHTTPRequestHandler):
+        def do_GET(self):
+            if self.path == '/' + os.path.basename(selected_file):
+                print(f" Request for {selected_file} received, sending the file...")
+                self.send_response(200)
+                self.end_headers()
+                with open(selected_file, 'rb') as file:
+                    self.wfile.write(file.read())
+                print(f"File {selected_file} sent, stopping the server...")
+                os._exit(0)  # Arrête le serveur
+            return http.server.SimpleHTTPRequestHandler.do_GET(self)
+
+            server_address = ("", selected_port)
+            httpd = http.server.HTTPServer(server_address, Requester)
+            httpd.serve_forever()
+    return Requester
+
+# Vérifier si le script est exécuté en tant que root
+def check_user_capabilities():
+    if os.geteuid() == 0:
+        return True
     else:
-        print(
-            bcolors.BOLD +
-            "Identifed following packages {} ".format(packages) +
-            "for module {}, select any one of them or choose ".format(dep) +
-            "to ingore or provide your own package name: " +
-            bcolors.ENDC
-        )
+        print("You must run this script with root privileges or assign the necessary capabilities to the Python binary.")
+        return False
 
-        options = packages
-        options.extend(extra_options)
-        menu = TerminalMenu(options)
-        selected_idx = menu.show()
+def MenuGeneral():
+    # Menu système d'exploitation de la cible :
+    style = ("bg_black", "fg_yellow", "bold")
+    ChoixCible = ["Linux", "Windows"]
+    terminal_menu = TerminalMenu(ChoixCible, menu_cursor="=>  ", menu_highlight_style=style, title="What is the target OS?")
+    menu_entry_index = terminal_menu.show()
+    OS = ChoixCible[menu_entry_index]
 
-        if selected_idx == -1 or selected_idx == len(options) - 1:
-            print(
-                bcolors.WARNING + "Module {} ignored.".format(dep) +
-                bcolors.ENDC
-            )
-
-            return ""
-
-        elif selected_idx == len(options) - 2:
-            pkg_name = ask_package_input(dep)
-            if pkg_name == "":
-                print(
-                    bcolors.WARNING +
-                    'Module {} ignored.'.format(dep) +
-                    bcolors.ENDC
-                )
-                return ""
+    # Menu choix de l'interface réseau
+    while True:
+        interfaces = ni.interfaces()
+        ip_mappings = []
+        for interface in interfaces:
+            ip_info = ni.ifaddresses(interface).get(ni.AF_INET)
+            if ip_info:
+                ip_address = ip_info[0]['addr']
+                ip_mappings.append(f"{interface} == {ip_address}")
             else:
-                print(
-                    bcolors.OKGREEN +
-                    "Using {} as package for module {}".format(pkg_name, dep) +
-                    "." + bcolors.ENDC
-                )
-                return pkg_name
+                ip_mappings.append(f"{interface} == No IP")
+
+        ip_menu = TerminalMenu(ip_mappings, menu_cursor="=>  ", menu_highlight_style=style, title="Select a network interface:")
+        ip_entry_index = ip_menu.show()
+        selected_ip = interfaces[ip_entry_index]
+        IPHOST = ni.ifaddresses(selected_ip).get(ni.AF_INET)
+        if not IPHOST:
+            print("/!\ Error: No IP address found for the selected interface. \nPlease select another interface.")
+            continue
+        IPHOST = IPHOST[0]['addr']
+        break
+
+    # Menu choix du répertoire
+    while True:
+        selected_directory = input("Enter the directory for file selection : ")
+        if os.path.isdir(selected_directory):
+            break
         else:
-            print(
-                bcolors.OKGREEN +
-                "Using {} as a package for module {}.".format(
-                    options[selected_idx],
-                    dep
-                ) +
-                bcolors.ENDC
-            )
-            return options[selected_idx]
- Narasimha1997/pyMigrate/env.py
- 
-def get_operator_management_subcommands():
-    @click.group(name="operator", cls=BentoctlCommandGroup)
-    def operator_management():
-        """
-        Sub-commands to install, list, uninstall and update operators.
+            print("The specified path is not a valid directory. Please try again.")
 
-        To see the list of all the operators available and their comparisons check out
-        <link to comparisons>.
-        """
+    # Menu choix du fichier à uploader
+    try:
+        selected_file = subprocess.check_output(f"cd {selected_directory} && fzf", shell=True).decode().strip()
+        selected_file = os.path.join(selected_directory, selected_file)
+        print(f"You have selected the file : {selected_file}!")
+    except subprocess.CalledProcessError as e:
+        print("/!\  Error: Unable to select a file. try again.")
+        sys.exit(1)
 
-    @operator_management.command(name="list")
-    def list_operator_command():  # pylint: disable=unused-variable
-        """
-        List all the available operators.
+    # Menu choix du port de téléchargement
+    ChoixPort = ["80", "8080", "8000", "443", "1234", "666", "Another port"]
+    Port_menu = TerminalMenu(ChoixPort, menu_cursor="=>  ", menu_highlight_style=style, title="Select a port : ")
+    Port_entry_index = Port_menu.show()
+    if ChoixPort[Port_entry_index] == "Another port":
+        selected_port = int(input("Please enter the port : "))
+        while check_port_in_use(IPHOST, selected_port):
+            print(f"The port {selected_port} is already in use or you do not have the necessary capabilities to use it.")
+            selected_port = int(input("Please enter another port : "))
+    else:
+        selected_port = int(ChoixPort[Port_entry_index])
+        if selected_port < 1024:
+            if not check_user_capabilities():
+                print(f"You do not have the necessary capabilities to use the port. {selected_port} !")
+                sys.exit(1)
+        while check_port_in_use(IPHOST, selected_port):
+            print(f"Le port {selected_port} is already in use ! ")
+            selected_port = int(input("Please enter another port : "))
 
-        Lists the operator, the path from where you can access operator locally and
-        if the operator was pulled from github, the github URL is also shown.
-        """
-        operators_list = local_operator_registry.list()
-        print_operator_list(operators_list)
+    return OS, IPHOST, selected_file, selected_port
 
-    @operator_management.command()
-    @click.argument("name", required=False)
-    @click.option("--version", "-v", type=click.STRING)
-    def install(name=None, version=None):  # pylint: disable=unused-variable
-        """
-        install operators.
+def main():
+    parser = argparse.ArgumentParser(description="Tool for quickly downloading files to a remote machine based on the target operating system. Launch the program and follow the prompts.")
+    args = parser.parse_args()
+    OS, IPHOST, selected_file, selected_port = MenuGeneral()
+    Syntaxe(OS, IPHOST, selected_file, selected_port)
 
-        There are 3 ways to install an operator into bentoctl and they are -
-
-        1. Interactive Mode: lists all official operators for user to choose from. Just
-           type `bentoctl operator install` to enter this mode.
-
-        2. Official operator: you can pass the name of one of the official operators
-           and the tool with fetch it for you.
-           eg. `bentoctl operator install aws-lambda`
-
-           Available operators: [aws-lambda, aws-sagemaker, aws-ec2, azure-functions,
-           azure-container-instances, google-compute-engine, google-cloud-run,
-           heroku]
-
-
-        3. Path: If you have the operator locally, either because you are building
-           our own operator or if cloning the operator from some other
-           remote repository then you can just pass the path
-           after the install command and it will register the local operator for you.
-           This is a special case since the operator will not have an associated URL
-           with it and hence cannot be updated using the tool.
-
-        """
-        if not name:
-            try:
-                from simple_term_menu import TerminalMenu
-
-                # show a simple menu with all the available official operators
-                available_operators = list(OFFICIAL_OPERATORS.keys())
-                tmenu = TerminalMenu(
-                    available_operators, title="Choose one of the Official Operators"
-                )
-                choice = tmenu.show()
-                name = available_operators[choice]
-                # When user uses the interactive mode, we will default to the latest
-                # version
-                version = None
-            except ImportError:
-                raise BentoctlException(
-                    "Please specify the name of the operator to install."
-                )
-        try:
-            operator_name = local_operator_registry.install_operator(name, version)
-            if operator_name is not None:
-                click.echo(f"Installed {operator_name}!")
-            else:
-                click.echo(
-                    f"Unable to install operator. `{name}` did not match any of the "
-                    "operator installation options. Check `bentoctl operator install "
-                    "--help` for mode details on how you can call this command."
-                )
-        except BentoctlException as e:
-            e.show()
-
-    @operator_management.command()
-    @click.option(
-        "-y",
-        "skip_confirm",
-        is_flag=True,
-        help="skip the prompt asking if you are sure.",
-    )
-    @click.argument("name", type=click.STRING)
-    def remove(name, skip_confirm):  # pylint: disable=unused-variable
-        """
-        [Deprecated] remove the given operator.
-
-        This will remove the operator from the list. If the operator was
-        installed locally this will not clear the codebase.
-        """
-        console.print(
-            "[red]This command will be removed soon"
-            "Use the 'operator uninstall' command.[/]"
-        )
-        uninstall_operator(name=name, skip_confirm=skip_confirm)
-
-    @operator_management.command()
-    @click.option(
-        "-y",
-        "skip_confirm",
-        is_flag=True,
-        help="skip the prompt asking if you are sure.",
-    )
-    @click.argument("name", type=click.STRING)
-    def uninstall(name, skip_confirm):  # pylint: disable=unused-variable
-        """
-        Uninstall the given operator.
-
-        This will remove the operator from the list. If the operator was
-        installed locally this will not clear the codebase.
-        """
-        uninstall_operator(name=name, skip_confirm=skip_confirm)
-
-    def uninstall_operator(name: str, skip_confirm: bool) -> None:
-        if not skip_confirm:
-            proceed_with_delete = Confirm.ask(
-                f"Are you sure you want to delete '{name}' operator"
-            )
-            if not proceed_with_delete:
-                return
-        try:
-            local_operator_registry.remove_operator(name)
-            click.echo(f"operator '{name}' removed!")
-        except BentoctlException as e:
-            e.show()
-
-    @operator_management.command()
-    @click.argument("name")
-    @click.option("--version", "-v", type=click.STRING)
-    def update(name, version):  # pylint: disable=unused-variable
-        """
-        Update the given operator to the latest version.
-
-        This only works for operators that have a URL associated with them. When passed
-        the name of an available operator it goes and fetches the latest code from
-        the Github repo and update the local codebase with it.
-        """
-        try:
-            if local_operator_registry.is_operator_on_latest_version(name):
-                click.echo(f"Operator '{name}' is already on the latest version.")
-            else:
-                local_operator_registry.update_operator(name, version)
-                click.echo(f"Operator '{name}' updated!")
-        except BentoctlException as e:
-            e.show()
-
-    return operator_management
- bentoml/bentoctl/operator_management.py
+if __name__ == "__main__":
+    main()
